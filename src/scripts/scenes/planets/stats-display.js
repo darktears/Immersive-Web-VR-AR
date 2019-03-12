b7ee955438c4ab4f990810a3730a717b960ff73b
Brooks Mikkelsen <bmikkelsen22@gmail.com>
2019-03-12 10:59:49 -0700

    built stats display that always stays in front of everything

--- src/scripts/scenes/planets/stats-display.js
+++ src/scripts/scenes/planets/stats-display.js
@@ -9,18 +9,16 @@ export function createStats(text, xOffset = 5) {
   const sprite = new TextSprite({
     material: {
       color: 'white',
-      fog: false
+      fog: false,
+      depthTest: false
     },
     redrawInterval: 250,
-    minFontSize: 1,
+    textSize: 1,
     texture: {
       fontFamily: 'Roboto, sans-serif',
       text
     }
   });
-
-  sprite.depthWrite = false;
-  sprite.renderOrder = -2;
 
   const animateFn = object => {
     sprite.position.copy(object.position);
